ba4185154036e19d8abdc394180ece6d770ddace
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:25:06 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 5')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #4: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Tenemos un Oponente en el Juego de la Vida')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Cada uno de nosotros tiene dos voces guiándonos en la vida: Nuestra Voz Verdadera nos anima a hacer esfuerzo, compartir, ocuparnos de nosotros mismos y de los demás.  Y nuestra Voz del Oponente que nos reta a tomar el camino fácil y pensar sólo en nosotros mismos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Somos los únicos que podemos escuchar lo que nuestras voces nos dicen.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Podemos distinguir nuestras dos voces preguntándonos, ¿Estoy pensando en mí y los demás? o ¿Estoy pensando sólo en mí?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Nuestra Voz del Oponente nos da la oportunidad de sobrepasar nuestro egoísmo y ganar el Juego de la Vida.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Voz Verdadera: La voz dentro de nosotros que nos anima a hacer un esfuerzo, compartir, pensar y ocuparme de mí mismo y de los demás.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Voz del Oponente: La voz dentro de nosotros que nos reta a tomar el camino fácil y ser egoístas.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento que elegiste escuchar a tu Voz Verdadera. ¿Qué pasó? ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento que elegiste escuchar a tu Voz del Oponente. ¿Qué pasó? ¿Cómo te sentiste?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 6 (Parte 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #5: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Podemos elegir escuchar a la Voz Verdadera en cualquier momento.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Somos los únicos que podemos elegir qué voz escuchamos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escuchar a nuestra Voz Verdadera nos lleva a tener plenitud espiritual duradera.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escuchar a nuestra Voz del Oponente nos lleva a tener satisfacción momentánea.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Plenitud espiritual duradera: El resultado de escuchar a tu Voz Verdadera; obliga a un esfuerzo pero se siente bien al pasar el tiempo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Satisfacción momentánea: El resultado de escuchar la Voz del Oponente puede ser que se sienta bien en el momento, pero será breve y se sentirá mal después.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • ¿Cuál es la diferencia entre plenitud duradera y satisfacción momentánea?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Qué decisiones has tomado últimamente que te han traído sólo satisfacción momentánea?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Qué decisiones has tomado últimamente que te han traído plenitud espiritual duradera?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 6 (Parte 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #5: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Podemos elegir escuchar a la Voz Verdadera en cualquier momento.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Somos los únicos que podemos elegir qué voz escuchamos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escuchar a nuestra Voz Verdadera nos lleva a tener plenitud espiritual duradera.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escuchar a nuestra Voz del Oponente nos lleva a tener satisfacción momentánea.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Plenitud espiritual duradera: El resultado de escuchar a tu Voz Verdadera; obliga a un esfuerzo pero se siente bien al pasar el tiempo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Satisfacción momentánea: El resultado de escuchar la Voz del Oponente puede ser que se sienta bien en el momento, pero será breve y se sentirá mal después.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • ¿Cuál es la diferencia entre plenitud duradera y satisfacción momentánea?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Qué decisiones has tomado últimamente que te han traído sólo satisfacción momentánea?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Qué decisiones has tomado últimamente que te han traído plenitud espiritual duradera?')),
+)
---
+++ videos.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("5.ogv",
+     _("Mi Oponente"),
+     _("Si se trata de comer comida chatarra, luchar con los amigos o hacer trampa en un examen, a menudo atrapamos a nuestros hijos haciendo cosas que saben que no deben hacer. En esta lección, van a descubrir qué es lo que los tienta a tomar decisiones en contra de sus mejores intereses."),
+         ),
+    ("6.1.ogv",
+     _("Mis Decisiones (Parte 1)"),
+     _("Nuestros hijos a menudo ponen la responsabilidad de su felicidad y satisfacción en sus amigos, profesores y con mayor frecuencia, en nosotros, sus padres. En esta lección, aprenderán que la plenitud duradera y la felicidad son el resultado de decisiones que toman para sí mismos."),
+    ),
+    ("6.4.ogv",
+     _("Mis Decisiones (Parte 2)"),
+     _("Nuestros hijos a menudo ponen la responsabilidad de su felicidad y satisfacción en sus amigos, profesores y con mayor frecuencia, en nosotros, sus padres. En esta lección, aprenderán que la plenitud duradera y la felicidad son el resultado de decisiones que toman para sí mismos."),
+    ),
+)
